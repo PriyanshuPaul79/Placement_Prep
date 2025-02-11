2cf49d369eee75719c7ca1424c6f2fb732fad2cb
7Priyanshu9 <priyanshupaul32@gmail.com>
2025-02-11 21:28:55 +0530

    Remove all occurances in substring

---
+++ Leetcode/removeAllOccurance.java
@@ -0,0 +1,10 @@
+public class removeAllOccurance {
+    public static String remove(String s, String part){
+        StringBuilder sb = new StringBuilder(s);
+        while(sb.indexOf(part)!=-1){
+            int idx = sb.indexOf(part);
+            sb.delete(idx, idx+part.length());
+        }
+        return sb.toString();
+    }
+}
